2e8a8e4311bb736158002940ad81e38a34201537
jess <jessachandler@gmail.com>
2024-12-24 08:35:55 -0800

    adds documentation for map_coverage and changes map to only color in completed portion rather than whole trail

--- functions/map_coverage.R
+++ functions/map_coverage.R
@@ -1,10 +1,39 @@
 # map your map
 
-map_coverage <- function(underlying_map, trail_coverage, 
-                         title, caption){
+#' map_coverage
+#' @description 
+#' Creates a visual representation of the portion 
+#' of trails covered overlaying the trail network
+#' 
+#' @param underlying_map sf that might represent the 
+#' area around your trail network. For the examples,
+#' I am using the Olympic Peninsula of Washington to 
+#' underly the Oly National Forest and Park trails
+#' @param trail_network sf the trail network data 
+#' same data as used as big_sf in the other functions.
+#' In the examples, this is sf from ONF.geojson and ONP.geojson
+#' @param trail_coverage sf output of get_coverage()
+#' @param trail_coverage_portion df has TRAIL_NAME 
+#' and portion covered; 
+#' second output of get_covered_portion()
+#' @param title string title you want for your map. 
+#' defaults to "Your Completed Trail Network"
+#' @param caption string caption you want for your map. 
+#' defaults to "created with outside_oly"
+map_coverage <- function(underlying_map, 
+                         trail_network,
+                         trail_coverage, 
+                         trail_coverage_portion,
+                         title = "Your Completed Trail Network", 
+                         caption = "created with outside_oly"){
+  
+  trails_w_color <- trail_coverage %>%
+    left_join(trail_coverage_portion)
+  
   ggplot() + 
     geom_sf(data = underlying_map, fill = "grey95") +
-    geom_sf(data = trail_coverage, aes(color = portion)) +
+    geom_sf(data = trail_network, color = "grey80") +
+    geom_sf(data = trails_w_color, aes(color = portion)) +
     scale_color_viridis_c(option = "turbo", 
                           na.value = "grey80", direction = -1,
                           breaks = 0.2*0:5, 
