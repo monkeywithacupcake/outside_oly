94d1e1878fc0ba8ce527f56057ce96990a08e10f
jess <jessachandler@gmail.com>
2024-12-24 09:06:35 -0800

    adds comments and greatly simplifies covereage functions

--- functions/get_coverage.R
+++ functions/get_coverage.R
@@ -1,5 +1,11 @@
 # get_coverage
 
+#' Get Coverage
+#' creates a sf df of the intersection of
+#' what has been completed and the trail network
+#' 
+#' @param big_sf sf trail network map
+#' @param little_sf sf trail gpx you completed
 get_coverage <- function(big_sf, little_sf){
   # id trails touched more than 50 m (so not just the cross)
   # assume big_sf and little_sf geometry is linestrings
@@ -13,31 +19,57 @@ get_coverage <- function(big_sf, little_sf){
   return(o)
 }
 
-# trail_coverage is output of get_coverage()
-get_covered_portion <- function(big_sf, trail_coverage, var, tolerance = 1){
+#' Get Covered Portion
+#' creates a dataframe of all trails in the trail
+#' network and identifies how much is completed
+#' 
+#' @param big_sf sf trail network map
+#' @param trail_coverage sf output of get_coverage()
+#' @param var string variable indicating trail name
+#' @param tolerance numeric value you believe indicates
+#' trail completion. defaults to 1, meaning 100% of official
+#' trail length covered. practical completions may be lower.
+#' @returns df one row for each row of big_sf with the
+#' portion covered in trail_coverage 
+get_covered_portion <- function(big_sf, 
+                                trail_coverage, 
+                                var = "TRAIL_NAME", 
+                                tolerance = 1){
   
   if(!"len" %in% names(big_sf)){
     big_sf  %>%
       mutate(len = as.numeric(st_length(geometry)))
   }
-  # returns list of total_complete and then complete by trail
-  p <- trail_coverage %>%
-    st_set_geometry(NULL)%>%
-    left_join(select(st_set_geometry(big_sf, NULL), 
-                     all_of(var), full_len = len)) %>%
+  
+  p <- select(st_set_geometry(big_sf, NULL), 
+              all_of(var), full_len = len) %>%
+    left_join(st_set_geometry(trail_coverage, NULL)) %>%
     mutate(portion = as.numeric(len/full_len),
            portion = if_else(portion > tolerance, 1, portion))
-  max_p <- p %>%
-    group_by(TRAIL_NAME) %>%
-    summarise(p = max(portion, na.rm=TRUE)) 
+  return(select(p, all_of(var), portion))
+}
+
+#' get_covered_percent
+#' @param big_sf sf trail network map
+#' @param covered_portion output of get_covered_portion()
+#' @param var string variable indicating trail name
+#' 
+#' @examples \dontrun{
+#' tc <- get_coverage(trail_network, my_gpx_lines)
+#' cp <- get_covered_portion(trail_network,tc, tolerance = 0.95)
+#' get_covered_percent(trail_network, cp)
+#' }
+get_covered_percent <- function(big_sf,
+                                covered_portion,
+                                var = "TRAIL_NAME"){
   tot_p <- select(st_set_geometry(big_sf, NULL), 
                   all_of(var), full_len = len) %>%
-    left_join(max_p) %>%
-    mutate(covered = full_len * p)
+    left_join(covered_portion) %>%
+    mutate(covered = full_len * portion)
   tot_p <- as.numeric(sum(tot_p$covered, na.rm= TRUE)/sum(tot_p$full_len))
   print(paste("Total coverage of", 
               scales::percent_format()(tot_p),
               "not including any double coverage")
   )
-  return(list(tot_p, select(p, all_of(var), portion)))
+  return(tot_p)
 }
