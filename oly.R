5c65f85a281bbc16dadb03173e3907d77d6be72f
jess <jessachandler@gmail.com>
2024-12-23 20:28:35 -0800

    makes oly example work

--- oly.R
+++ oly.R
@@ -6,34 +6,8 @@ for(fp in list.files("functions")){
 }
 
 # read in the onp and onf geodata
-
-if(file.exists(file.path("data", "ONP.geojson"))){
-  onp<- read_sf(file.path("data", "ONP.geojson"))
-} else {
-  onp_raw <- read_sf(file.path("data", "OlympicTrailData.json"))
-  onp <- onp_raw %>%
-    group_by(TRAIL_NAME = TRLNAME) %>%
-    dplyr::summarize(do_union=FALSE) %>%  # do_union=FALSE doesn't work as well
-    st_cast("MULTILINESTRING") %>%
-    ungroup() %>%
-    mutate(len = as.numeric(st_length(geometry)))
-  write_sf(onp, file.path("data", "ONP.geojson"))
-}
-
-if(file.exists(file.path("data", "ONF.geojson"))){
-  onf <- read_sf(file.path("data", "ONF.geojson"))
-} else {
-  onf_raw <- read_sf(file.path("data", "National_Forest_System_Trails_(Feature_Layer).geojson"))
-  onf_raw <- onf_raw %>%
-    filter(grepl("^0609", ADMIN_ORG)) 
-  onf <- onf_raw %>%
-    group_by(TRAIL_NAME) %>%
-    dplyr::summarize(do_union=FALSE) %>%  # do_union=FALSE doesn't work as well
-    st_cast("MULTILINESTRING") %>%
-    ungroup() %>%
-    mutate(len = as.numeric(st_length(geometry)))
-  write_sf(onf, file.path("data", "ONF.geojson"))
-}
+onp<- read_sf(file.path("data", "ONP.geojson"))
+onf <- read_sf(file.path("data", "ONF.geojson"))
 
 # get a cropped map of wa
 bbox <- get_bbox(onp, onf)
@@ -42,6 +16,7 @@ wa_cropped <- get_op_boundary(bbox)
 
 # read in hikes
 # you will need to put a .gpx in /data/done
+# i put one hike in there for it to do something
 done <- read_gpx_in_folder(file.path("data", "done"))
 done <- sf::st_crop(done, bbox)
 done_lines <- done %>%
@@ -63,7 +38,7 @@ onf_w_me <- onf %>%
 w_me <- onp_w_me %>%
   bind_rows(onf_w_me)
 
-for(yr in 2022:2024){
+for(yr in unique(done_lines$yr)){
   done_this_yr <- filter(done_lines, yr == yr)
   onp_coverage <- get_coverage(onp, done_this_yr)
   oo <- get_covered_portion(onp, onp_coverage, "TRAIL_NAME", .95)
@@ -84,33 +59,4 @@ for(yr in 2022:2024){
                                scales::percent(oof[[1]]), "ONF"))
   print(p)
 }
-
-
-ggplot() + 
-  geom_sf(data = wa_cropped, fill = "grey95") +
-  geom_sf(data = w_me, aes(color = portion)) +
-  scale_color_viridis_c(option = "turbo", 
-                        na.value = "grey80", direction = -1,
-                        breaks = 0.2*0:5, labels = scales::percent(0.2*0:5) ) +
-  theme_void() +
-  labs(title = "Olympic National Forest & Park covered 2022-2024",
-       color = "", 
-       caption = paste("official trails, together: ", 
-                       scales::percent(oo[[1]]), "ONP &",
-                       scales::percent(oof[[1]]), "ONF")
-       
-       )
-
-
-
-+
-  geom_label_repel(data = filter(w_me, portion ==1), 
-                   size = 1,
-                   stat = "sf_coordinates",
-                   aes(geometry = geometry, label = TRAIL_NAME),
-                   min.segment.length = 1)
-
-
-
-
 
